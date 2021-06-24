b54e698017eddc5274d77008d84dec2bc70213cf
Heli Ribeiro <34356621+heliribeiro@users.noreply.github.com>
2021-06-24 14:43:05 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 mongoose.connect(
-  'mongodb+srv://heliribeiro:heliribeiro@devcluster.q6hvq.mongodb.net/devFlix?retryWrites=true&w=majority',
+  '',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
